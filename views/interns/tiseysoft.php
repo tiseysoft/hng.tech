07703f0ed1ca63a85fb7de83182285a23908ffe7
tiseysoft <tiseysoft@yahoo.com>
2019-04-05 22:42:36 -0700

    my profile page

---
+++ views/interns/tiseysoft.php
@@ -0,0 +1,37 @@
+html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/iambeejayayo/image/upload/v1547954566/fav-500.png" alt="Mark Essien">
+                        <h6>Tisey Okuwobi</h6>
+                        <p>Back end engr</p>
+                        <ul class="list">
+                           <li><a href="www.twitter.com/tiseysoft" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="www.github.com/tiseysoft" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="www.meduim.com/tiseysoft" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am TiseySoft, a backend deverloper , specialist in creating web apps in PHP and MySQL.. I play video games and writer music when I am not coding.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP | MySQL | MEAN stack.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
